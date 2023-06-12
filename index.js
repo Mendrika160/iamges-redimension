67b47c8fe85591537374721acbac6fdae82f0cfb
mendrika <mendrika@debian>
2023-06-12 20:15:38 +0200

    add command functionality

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 const express = require('express');
 const fs = require('fs');
 const resizedImage = require('./middleware/resizedImage');
@@ -6,14 +8,14 @@ require('dotenv').config();
 const app = express();
 
 // Get the desired width and height from the command line arguments
-// const [width, height] = process.argv[2].split('*').map(Number);
-// // Get the source and destination paths from the command line arguments
-// const sourcePath = process.argv[3];
-// const destinationPath = process.argv[4];
-const sourcePath = './img';
-const destinationPath = './destination';
-const width = 500;
-const height = 500;
+const [width, height] = process.argv[2].split('*').map(Number);
+// Get the source and destination paths from the command line arguments
+const sourcePath = process.argv[3];
+const destinationPath = process.argv[4];
+// const sourcePath = './img';
+// const destinationPath = './destination';
+// const width = 500;
+// const height = 500;
 // // Check if the source path is a directory or a file
 if (fs.lstatSync(sourcePath).isDirectory()) {
     // If it's a directory, call the resizeImagesInDirectory function to resize all images in that directory
